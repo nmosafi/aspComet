636f6a0c0a406ba385c8a05da5fedd4247324d2c
Robert Golias <Robert.Golias@gmail.com>
2010-10-03 20:09:30 +0100

    Fix in SwallowHandler.cs
    The handler was returning null, which is not handled by MessagesProcessor.
    The modified code instead returns result with null message in case of success, and sends a failed response if the publishing event has been cancelled.

--- src/AspComet.Specifications/MessageHandlers/SwallowHandlerSpecs.cs
+++ src/AspComet.Specifications/MessageHandlers/SwallowHandlerSpecs.cs
@@ -18,7 +18,42 @@ namespace AspComet.Specifications.MessageHandlers
         It should_raise_a_published_event_with_the_request_message =()=>
             eventHubMonitor.RaisedEvent<PublishingEvent>().Message.ShouldEqual(request);
 
-        It should_not_return_a_result = () =>
-            result.ShouldBeNull();
+        It should_not_return_a_response_message = () =>
+            result.Message.ShouldBeNull();
+
+        It should_specify_that_the_result_cannot_be_treated_as_a_long_poll = () =>
+            result.CanTreatAsLongPoll.ShouldBeFalse();
     }
+
+    public class when_swallowing_a_message_and_the_publishing_event_is_cancelled : MessageHandlerScenario<SwallowHandler>
+    {
+        private const string CancellationReason = "ThisIsTheCancellationReasonForSwallowing";
+
+        Establish context = () => {
+            EventHub.Subscribe<PublishingEvent>(x =>
+            {
+                x.Cancel = true;
+                x.CancellationReason = CancellationReason;
+            });
+            eventHubMonitor.StartMonitoring<PublishingEvent>();
+        };
+
+        Because of = () =>
+            result = SUT.HandleMessage(request);
+
+        Behaves_like<ItHasHandledAMessage> has_handled_a_message;
+
+        It should_raise_a_published_event_with_the_request_message =()=>
+            eventHubMonitor.RaisedEvent<PublishingEvent>().Message.ShouldEqual(request);
+
+        It should_send_an_unsuccessful_response = () =>
+            result.Message.successful.ShouldEqual( false );
+
+        It should_send_an_unsuccessful_response_with_error_equal_to_cancellation_reason = () =>
+            result.Message.error.ShouldEqual( CancellationReason );
+
+        It should_specify_that_the_result_cannot_be_treated_as_a_long_poll = () =>
+            result.CanTreatAsLongPoll.ShouldBeFalse();
+    }
+
 }
